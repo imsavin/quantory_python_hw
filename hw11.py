dc82f53a75fed20b00a8513e530abfd9d4f6457c
Ilya Savin <im.savin@mail.ru>
2021-08-02 21:55:19 +0300

    add hw11

---
+++ hw11.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Savin Ilya Python Homework N11
+
+# -------
+# Напишите функцию letters_range, которая ведет себя
+# похожим на range образом, однако в качестве start и
+# stop принимает не числа, а буквы латинского алфавита
+# (в качестве step принимает целое число) и возращает
+# не перечисление чисел, а список букв, начиная с
+# указанной в качестве start, до указанной в качестве
+# stop с шагом step (по умолчанию равным 1).
+# -----
+
+
+def letters_range(first_letter, second_letter, step=1):
+    letters_list = []
+    if all(i in range(ord('a'), ord('z')+1) for i in (ord(first_letter), ord(second_letter))):
+        for i in range(ord(first_letter), ord(second_letter), step):
+            letters_list.append(chr(i))
+    return letters_list
