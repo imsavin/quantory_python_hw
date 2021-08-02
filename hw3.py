623cbff5ab867ab7b91ade3e8fe098ec5bb53427
Ilya Savin <im.savin@mail.ru>
2021-08-02 21:50:44 +0300

    added descriptions

--- hw3.py
+++ hw3.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 # Savin Ilya Python Homework N3
 
+# -------
+# 1. После запуска предлагает пользователю ввести текст.
+# 2. В качестве ответа печатает наиболее часто встречающееся в тексте слово
+# или несколько таких слов, если имеет место "ничья". Также указывая
+# сколько именно раз слово встретилось в тексте. (Игнорируйте заглавные буквы
+# при отождествлении слов - то есть считайте слова "Подлодка" и "подлодка"
+# одинаковыми, а разные формы слов - разными словами)
+# После чего ждет следующего ввода.
+# --------
 
 from collections import Counter
 
-input_line = input("Enter string: ")
-input_list = input_line.lower().split()
-dict_counter = Counter(input_list)
-max_count = max(dict_counter.values())
-for k, v in dict_counter.items():
-    if v == max_count:
-        print(str(v) + " - " + k)
+while true:
+    input_line = input("Enter string: ")
+    input_list = input_line.lower().split()
+    dict_counter = Counter(input_list)
+    max_count = max(dict_counter.values())
+    for k, v in dict_counter.items():
+        if v == max_count:
+            print(str(v) + " - " + k)
--- hw7.py
+++ hw7.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 # Savin Ilya Python Homework N7
 
+# ---------
+# Напишите программу, которая читает данные из файлов
+# /etc/passwd и /etc/group на вашей системе и выводит
+# следующую информацию в файл output.txt:
+# 1. Количество пользователей, использующих все имеющиеся
+# интерпретаторы-оболочки.
+# ( /bin/bash - 8 ; /bin/false - 11 ; ... )
+# 2. Для всех групп в системе - UIDы пользователей
+# состоящих в этих группах.
+# ( root:1, sudo:1001,1002,1003, ...)
+# ----------
 
 pw_file = open("passwd", 'r')
 gr_file = open("group", 'r')
@@ -14,6 +25,7 @@ for line in pw_file:
     users_dict[pw_list[0].strip("\n")] = pw_list[2].strip("\n")
 
 print(shells_dict)
+print()
 
 pw_file.close()
 tmp_line = ""
